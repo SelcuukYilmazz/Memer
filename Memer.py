928a5f53f84d07926b2216c860d6500e22787914
Selçuk Yılmaz <selcukyilmaz2000@hotmail.com>
2021-02-05 10:55:13 +0300

    Add files via upload

---
+++ Memer.py
@@ -0,0 +1,110 @@
+from webbrowser import*
+from random import*
+while 1:
+    input("enter tuşuna bas sihri gör :D")
+    a = randrange(1,51)
+    if a == 1:
+        open("https://www.instagram.com/p/B4EV9HEFKps/")
+    elif a == 2:
+        open("https://www.instagram.com/p/B4FRChLpW4C/")
+    elif a == 3:
+        open("https://www.instagram.com/p/B0gdG_JJLBd/")
+    elif a == 4:
+        open("https://www.instagram.com/p/B32WnftiThl/")
+    elif a == 5:
+        open("https://www.instagram.com/p/B2eusXypyFp/")
+    elif a == 6:
+        open("https://www.instagram.com/p/B2eYSvQJi5r/")
+    elif a == 7:
+        open("https://www.instagram.com/p/B4VeHYDnkix/")
+    elif a == 8:
+        open("https://www.instagram.com/p/B4VRm4EJk8v/")
+    elif a == 9:
+        open("https://www.youtube.com/watch?v=iDVItAX-meU")
+    elif a == 10:
+        open("https://www.instagram.com/p/B3wwAWQJCzb/")
+    elif a == 11:
+        open("https://www.instagram.com/p/B3SBB3Hpfoh/")
+    elif a == 12:
+        open("https://www.instagram.com/p/B3KWGBcphgp/")
+    elif a == 13:
+        open("https://www.youtube.com/watch?v=bFvqf8Z13PU")
+    elif a == 14:
+        open("https://www.youtube.com/watch?v=q0anpS99kGk")
+    elif a == 15:
+        open("https://www.youtube.com/watch?v=z3GfsqmyLew")
+    elif a == 16:
+        open("https://www.youtube.com/watch?v=QzPxnHIkhUs")
+    elif a == 17:
+        open("https://www.youtube.com/watch?v=wgUczLEUWkA")
+    elif a == 18:
+        open("https://www.youtube.com/watch?v=_15UrPHkVQo")
+    elif a == 19:
+        open("https://www.youtube.com/watch?v=P3ZON8DUPys")
+    elif a == 20:
+        open("https://www.youtube.com/watch?v=dO0FQUMQhPY")
+    elif a == 21:
+        open("https://www.youtube.com/watch?v=W6oQUDFV2C0")
+    elif a == 22:
+        open("https://www.instagram.com/p/B3bq1CMHeuZ/")
+    elif a == 23:
+        open("https://www.instagram.com/p/B4e0ckphdRA/")
+    elif a == 24:
+        open("https://www.instagram.com/p/B4cToP-h08r/")
+    elif a == 25:
+        open("https://www.instagram.com/p/B4R2Ka0BXPg/")
+    elif a == 26:
+        open("https://www.instagram.com/p/B3hk_3th63B/")
+    elif a == 27:
+        open("https://www.instagram.com/p/B29FlE1DY3d/")
+    elif a == 28:
+        open("https://www.instagram.com/p/B2J2dF5HkKS/")
+    elif a == 29:
+        open("https://www.instagram.com/p/B2UMGlTDMkE/")
+    elif a == 30:
+        open("https://www.instagram.com/p/B16UrgrlD46/")
+    elif a == 31:
+        open("https://www.instagram.com/p/B4U2jqmpAwz/")
+    elif a == 32:
+        open("https://www.instagram.com/p/B4SFOSQpgTl/")
+    elif a == 33:
+        open("https://www.instagram.com/p/B3_oFKeJ3cb/")
+    elif a == 34:
+        open("https://www.instagram.com/p/B17_SzKAU2B/")
+    elif a == 35:
+        open("https://www.instagram.com/p/B1ZuQfzHV1U/")
+    elif a == 36:
+        open("https://www.instagram.com/p/B1RbU76lD7W/")
+    elif a == 37:
+        open("https://www.instagram.com/p/B0_14KaFlA8/")
+    elif a == 38:
+        open("https://www.instagram.com/p/B01FdHvnb9W/")
+    elif a == 39:
+        open("https://www.instagram.com/p/B0vwNhvBc-Q/")
+    elif a == 40:
+        open("https://www.youtube.com/watch?v=HttF5HVYtlQ")
+    elif a == 41:
+        open("https://www.youtube.com/watch?v=uZY9Sc1n1Mk")
+    elif a == 42:
+        open("https://www.instagram.com/p/BzPqaCClYZA/")
+    elif a == 43:
+        open("https://www.instagram.com/p/BzYd7F_AsbO/")
+    elif a == 44:
+        open("https://www.instagram.com/p/ByZ3k30l6Ar/")
+    elif a == 45:
+        open("https://www.instagram.com/p/ByAvJs6lhx_/")
+    elif a == 46:
+        open("https://www.instagram.com/p/BxuSroclIG1/")
+    elif a == 47:
+        open("https://www.instagram.com/p/BxXhuLrHyYg/")
+    elif a == 48:
+        open("https://www.instagram.com/p/BxT_GZDlMMG/")
+    elif a == 49:
+        open("https://www.instagram.com/p/Bw5DnNyhPaj/")
+    elif a == 50:
+        open("https://www.instagram.com/p/BwHUg5lgMLC/")
+    k = input("devam etmek için 1 değerini girin")
+    if k == "1":
+        continue
+    else:
+        break
